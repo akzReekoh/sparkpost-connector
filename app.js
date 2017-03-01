d58bc4bf4140ce248339474342f4b85dff09ddc2
Achilles Dinglasan <adinglasan@reekoh.com>
2017-03-01 15:10:26 +0800

    migrated to v3

--- app.js
+++ app.js
@@ -1,72 +1,75 @@
-'use strict';
+'use strict'
 
-var platform = require('./platform'),
-    isEmpty = require('lodash.isempty'),
-    isPlainObject = require('lodash.isplainobject'),
-    isArray = require('lodash.isarray'),
-    async = require('async'),
-    config,
-    sparkPostClient;
+let reekoh = require('reekoh')
+let _plugin = new reekoh.plugins.Connector()
+let async = require('async')
+let isArray = require('lodash.isarray')
+let isEmpty = require('lodash.isempty')
+let isPlainObject = require('lodash.isplainobject')
+let sparkPostClient = null
 
 let sendData = (data, callback) => {
-    if(isEmpty(data.template_id))
-        data.template_id = config.default_template_id;
+  if (isEmpty(data.templateId)) { data.templateId = _plugin.config.defaultTemplateId }
 
-    if(isEmpty(data.recipients))
-        data.recipients = config.default_recipients;
+  if (isEmpty(data.recipients)) {
+    data.recipients = _plugin.config.defaultRecipients
+  }
 
-    sparkPostClient.sendMail({
-        content : {template_id: data.template_id},
-        recipients : data.recipients
-    }, function(error, info){
-        if(!error){
-            platform.log(JSON.stringify({
-                title: 'SparkPost Email sent.',
-                data: data
-            }));
-        }
-
-        callback(error);
-    });
-};
-
-platform.on('data', function (data) {
-    if(isPlainObject(data)){
-        sendData(data, (error) => {
-            if(error) {
-                console.error(error);
-                platform.handleException(error);
-            }
-        });
+  sparkPostClient.sendMail({
+    content: {templateId: data.templateId},
+    recipients: data.recipients
+  }, function (error) {
+    if (!error) {
+      _plugin.log(JSON.stringify({
+        title: 'SparkPost Email sent.',
+        data: data
+      }))
     }
-    else if(isArray(data)){
-        async.each(data, (datum, done) => {
-            sendData(datum, done);
-        }, (error) => {
-            if(error) {
-                console.error(error);
-                platform.handleException(error);
-            }
-        });
-    }
-    else
-        platform.handleException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data));
-});
 
-platform.once('close', function () {
-    platform.notifyClose();
-});
+    callback(error)
+  })
+}
 
-platform.once('ready', function (options) {
-    config = options;
+/**
+ * Emitted when device data is received.
+ * This is the event to listen to in order to get real-time data feed from the connected devices.
+ * @param {object} data The data coming from the device represented as JSON Object.
+ */
+_plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    sendData(data, (error) => {
+      if (error) {
+        console.error(error)
+        _plugin.logException(error)
+      }
+    })
+  } else if (isArray(data)) {
+    async.each(data, (datum, done) => {
+      sendData(datum, done)
+    }, (error) => {
+      if (error) {
+        console.error(error)
+        _plugin.logException(error)
+      }
+    })
+  } else {
+    _plugin.logException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data))
+  }
+})
 
-    var nodemailer = require('nodemailer');
-    var sparkPostTransport = require('nodemailer-sparkpost-transport');
+/**
+ * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
+ */
+_plugin.once('ready', () => {
+  let nodemailer = require('nodemailer')
+  let sparkPostTransport = require('nodemailer-sparkpost-transport')
 
-    sparkPostClient = nodemailer.createTransport(sparkPostTransport({
-        sparkPostApiKey: options.api_key
-    }));
+  sparkPostClient = nodemailer.createTransport(sparkPostTransport({
+    sparkPostApiKey: _plugin.config.apiKey
+  }))
 
-	platform.notifyReady();
-	platform.log('Connector has been initialized.');
-});
+  _plugin.log('SparkPost Connector has been initialized.')
+  _plugin.emit('init')
+})
+
+module.exports = _plugin
--- test/sparkpost-connector.test.js
+++ test/sparkpost-connector.test.js
@@ -1,108 +1,184 @@
-'use strict';
+'use strict'
 
-const API_KEY = '30f92d579144b92f77a6fe75ddc1c2e8e5ab1112';
+const amqp = require('amqplib')
 
-var cp     = require('child_process'),
-	assert = require('assert'),
-	connector;
+const API_KEY = '30f92d579144b92f77a6fe75ddc1c2e8e5ab1112'
 
-describe('Connector', function () {
-	this.slow(5000);
+let _channel = null
+let _conn = null
+let app = null
 
-	after('terminate child process', function (done) {
-        this.timeout(7000);
+describe('SparkPost Connector Test', () => {
+  before('init', () => {
+    process.env.ACCOUNT = 'adinglasan'
+    process.env.CONFIG = JSON.stringify({
+      apiKey: API_KEY,
+      defaultRecipients: [
+        {
+        'address': {
+          'email': 'adinglasan@reekoh.com',
+          'name': 'Achilles Dinglasan'
+          }
+        }
+      ],
+      defaultTemplateId: 'my-first-email'
+    })
+    process.env.INPUT_PIPE = 'ip.sparkpost'
+    process.env.LOGGERS = 'logger1, logger2'
+    process.env.EXCEPTION_LOGGERS = 'ex.logger1, ex.logger2'
+    process.env.BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-        setTimeout(function(){
-            connector.kill('SIGKILL');
-            done();
-        }, 5000);
-	});
+    amqp.connect(process.env.BROKER)
+      .then((conn) => {
+        _conn = conn
+        return conn.createChannel()
+      }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			assert.ok(connector = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+  after('close connection', function (done) {
+    _conn.close()
+    done()
+  })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 5 seconds', function (done) {
-			this.timeout(5000);
+  describe('#start', function () {
+    it('should start the app', function (done) {
+      this.timeout(10000)
+      app = require('../app')
+      app.once('init', done)
+    })
+  })
 
-			connector.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-			});
+  describe('#data', () => {
+    it('should send data to third party client', function (done) {
+      this.timeout(15000)
 
-			connector.send({
-				type: 'ready',
-				data: {
-					options: {
-						api_key: API_KEY,
-						default_recipients: [
-                            {
-                                'address': {
-                                    'email': 'adinglasan@reekoh.com',
-                                    'name': 'Achilles Dinglasan'
-                                }
-                            }
-                        ],
-                        default_template_id: 'my-first-email'
-					}
-				}
-			}, function (error) {
-				assert.ifError(error);
-			});
-		});
-	});
+      let data = {
+        recipients: [
+            {
+              'address': {
+                'email': 'akzdinglasan@gmail.com',
+                'name': 'Achilles Dinglasan'
+              }
+            }
+        ],
+        templateId: 'my-first-email'
+      }
 
-	describe('#data', function () {
-		it('should process the JSON data', function (done) {
-            connector.send({
-                type: 'data',
-                data: {
-                    recipients: [
-                        {
-                            'address': {
-                                'email': 'akzdinglasan@gmail.com',
-                                'name': 'Achilles Dinglasan'
-                            }
-                        }
-                    ],
-                    template_id: 'my-first-email'
-                }
-            }, done);
-		});
-	});
+      _channel.sendToQueue('ip.sparkpost', new Buffer(JSON.stringify(data)))
+      setTimeout(done, 10000)
+    })
+  })
+})
 
-	describe('#data', function () {
-		it('should process the Array data', function (done) {
-			connector.send({
-				type: 'data',
-				data:[
-					{
-						recipients: [
-							{
-								'address': {
-									'email': 'akzdinglasan@gmail.com',
-									'name': 'Achilles Dinglasan'
-								}
-							}
-						],
-						template_id: 'my-first-email'
-					},
-					{
-						recipients: [
-							{
-								'address': {
-									'email': 'akzdinglasan@gmail.com',
-									'name': 'Achilles Dinglasan'
-								}
-							}
-						],
-						template_id: 'my-first-email'
-					}
-				]
-			}, done);
-		});
-	});
-});
+// 'use strict';
+//
+// const API_KEY = '30f92d579144b92f77a6fe75ddc1c2e8e5ab1112';
+//
+// var cp     = require('child_process'),
+// 	assert = require('assert'),
+// 	connector;
+//
+// describe('Connector', function () {
+// 	this.slow(5000);
+//
+// 	after('terminate child process', function (done) {
+//         this.timeout(7000);
+//
+//         setTimeout(function(){
+//             connector.kill('SIGKILL');
+//             done();
+//         }, 5000);
+// 	});
+//
+// 	describe('#spawn', function () {
+// 		it('should spawn a child process', function () {
+// 			assert.ok(connector = cp.fork(process.cwd()), 'Child process not spawned.');
+// 		});
+// 	});
+//
+// 	describe('#handShake', function () {
+// 		it('should notify the parent process when ready within 5 seconds', function (done) {
+// 			this.timeout(5000);
+//
+// 			connector.on('message', function (message) {
+// 				if (message.type === 'ready')
+// 					done();
+// 			});
+//
+// 			connector.send({
+// 				type: 'ready',
+// 				data: {
+// 					options: {
+// 						api_key: API_KEY,
+// 						default_recipients: [
+//                             {
+//                                 'address': {
+//                                     'email': 'adinglasan@reekoh.com',
+//                                     'name': 'Achilles Dinglasan'
+//                                 }
+//                             }
+//                         ],
+//                         default_template_id: 'my-first-email'
+// 					}
+// 				}
+// 			}, function (error) {
+// 				assert.ifError(error);
+// 			});
+// 		});
+// 	});
+//
+// 	describe('#data', function () {
+// 		it('should process the JSON data', function (done) {
+//             connector.send({
+//                 type: 'data',
+//                 data: {
+//                     recipients: [
+//                         {
+//                             'address': {
+//                                 'email': 'akzdinglasan@gmail.com',
+//                                 'name': 'Achilles Dinglasan'
+//                             }
+//                         }
+//                     ],
+//                     template_id: 'my-first-email'
+//                 }
+//             }, done);
+// 		});
+// 	});
+//
+// 	describe('#data', function () {
+// 		it('should process the Array data', function (done) {
+// 			connector.send({
+// 				type: 'data',
+// 				data:[
+// 					{
+// 						recipients: [
+// 							{
+// 								'address': {
+// 									'email': 'akzdinglasan@gmail.com',
+// 									'name': 'Achilles Dinglasan'
+// 								}
+// 							}
+// 						],
+// 						template_id: 'my-first-email'
+// 					},
+// 					{
+// 						recipients: [
+// 							{
+// 								'address': {
+// 									'email': 'akzdinglasan@gmail.com',
+// 									'name': 'Achilles Dinglasan'
+// 								}
+// 							}
+// 						],
+// 						template_id: 'my-first-email'
+// 					}
+// 				]
+// 			}, done);
+// 		});
+// 	});
+// });
